bedf3f1341ce6c889c7453ffe37b6b33aee6451c
shun-yu <91871976+shun-yu@users.noreply.github.com>
2021-10-06 11:51:45 +0900

    update views.py

--- polls/views.py
+++ polls/views.py
@@ -54,7 +54,3 @@ def vote(request, question_id):
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
-import logging
-logger = logging.getLogger("azure”)
-logger.warning("this will be tracked")
